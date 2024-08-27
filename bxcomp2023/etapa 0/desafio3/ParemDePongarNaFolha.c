57539b868ec8e68ccb7738ec0cde31cc9d8c2266
JArroioS <49651793+JArroioS@users.noreply.github.com>
2024-08-27 15:44:14 -0300

    Correção do desafio 3

--- bxcomp2023/etapa 0/desafio3/ParemDePongarNaFolha.c
+++ bxcomp2023/etapa 0/desafio3/ParemDePongarNaFolha.c
@@ -5,6 +5,8 @@ int main()
     int numeroCasos;
     int quantidadePartidas;
     int pontuacaoJogador1, pontuacaoJogador2, vitoriasJogador1, vitoriasJogador2;
+    
+    scanf("%d",&numeroCasos);
 
     for(int i = 0; i<numeroCasos; i++)
     {
@@ -16,34 +18,30 @@ int main()
             scanf("%d %d",&pontuacaoJogador1,&pontuacaoJogador2);
             if(pontuacaoJogador1 > pontuacaoJogador2)
             {
-                printf("%d-1 ",&pontuacaoJogador1);
+                printf("%d-1 ",pontuacaoJogador1);
                 vitoriasJogador1++;
             }
             else if(pontuacaoJogador2 > pontuacaoJogador1)
             {
-                printf("%d-2 ",&pontuacaoJogador2);
+                printf("%d-2 ",pontuacaoJogador2);
                 vitoriasJogador2++;
             }
             else
             {
-                printf("%d-Empate ",&pontuacaoJogador1);
+                printf("%d-Empate ",pontuacaoJogador1);
             }
-
-            if(vitoriasJogador1 > vitoriasJogador2)
-            {
-                printf("Resultado: 1\n");
-            }
-            else if(vitoriasJogador2 > vitoriasJogador1)
-            {
-                printf("Resultado: 2\n");
-            }
-            else
-            {
-                printf("Resultado: Empate\n");
-            }
-            
         }
-
-
+        if(vitoriasJogador1 > vitoriasJogador2)
+        {
+            printf("Resultado: 1\n");
+        }
+        else if(vitoriasJogador2 > vitoriasJogador1)
+        {
+            printf("Resultado: 2\n");
+        }
+        else
+        {
+            printf("Resultado: Empate\n");
+        }
     }
 }
