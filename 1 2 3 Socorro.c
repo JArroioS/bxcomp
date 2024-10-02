bbb3622be165bbe4b3ceb1980eb8512dd0858539
alissonsz1 <42107837+alissonsz1@users.noreply.github.com>
2024-10-02 15:20:36 -0300

    Add files via upload
    4ª Etapa - Desafio 1 - 2023

---
+++ 1 2 3 Socorro.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main() {
+  int casos;
+  int matriz[7][7] = {0}; //zera matriz
+
+  scanf("%d", &casos);
+
+  while(casos--){
+
+    for(int i = 0; i < 7; i++){
+      for(int j = 0; j < 7; j++){
+        scanf("%d", &matriz[i][j]);
+      }
+    }
+
+    for(int i = 0; i < 7; i++){
+      for(int j = 0; j < 7; j++){
+        matriz[j][i] = matriz[i][j];
+      }
+    }
+
+    printf("\n");
+
+    for(int i = 0; i < 7; i++){
+      for(int j = 0; j < 7; j++){
+        printf("%d " , matriz[i][j]);
+      }
+
+      printf("\n");
+    }
+
+    printf("\n");
+  }
+    return 0;
+}
