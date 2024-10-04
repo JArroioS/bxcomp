c5f8bc8b5560e678cc9a320f9dbd3dc4f54894c6
alissonsz1 <42107837+alissonsz1@users.noreply.github.com>
2024-10-04 10:43:20 -0300

    Update 1 2 3 Socorro.c

--- 1 2 3 Socorro.c
+++ 1 2 3 Socorro.c
@@ -10,15 +10,15 @@ int main() {
 
   while(casos--){
 
-    for(int i = 0; i < 7; i++){
+    for(int i = 0; i < 7; i++){ //popula a matriz
       for(int j = 0; j < 7; j++){
         scanf("%d", &matriz[i][j]);
       }
     }
 
-    for(int i = 0; i < 7; i++){
+    for(int i = 0; i < 7; i++){ 
       for(int j = 0; j < 7; j++){
-        matriz[j][i] = matriz[i][j];
+        matriz[j][i] = matriz[i][j]; //pega a diagonal de cima e copia para a diagonal de baixo
       }
     }
 
